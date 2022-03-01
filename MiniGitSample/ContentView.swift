84e11e701e85222a05cfb3f95a03a05930e5472f
Lightech <lightech@outlook.com>
2022-02-28 21:10:06 -0500

    Add cloning example

--- MiniGitSample/ContentView.swift
+++ MiniGitSample/ContentView.swift
@@ -6,11 +6,30 @@
 //
 
 import SwiftUI
+import MiniGit
+
+let documentURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+
+let localRepoLocation = documentURL.appendingPathComponent("BigMac")
+
+let remoteRepoLocation = "https://github.com/light-tech/BigMac.git"
+
+// Do not do this in a real application, put the credentials somewhere safe
+// And possibly encrypt them or keychain them by subclassing CredentialsManager
+let credentialManager = CredentialsManager(credentialsFileUrl: documentURL.appendingPathComponent("gitcredentials"))
 
 struct ContentView: View {
+
+    @State var message = ""
+    @StateObject var repo = GitRepository(localRepoLocation, credentialManager)
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            // On Mac Catalyst, after clicking on this button, you can go to ~/Documents/ and checkout if there is the repository BigMac
+            Button("Clone remote Git repo") {
+                repo.clone(remoteRepoLocation)
+            }
+        }.padding(5)
     }
 }
 
